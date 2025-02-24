c57e809bfd5a7b76b09ab101a3ca505a96ae6c3c
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-02-23 20:39:04 -0600

    Grid added

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       </h1>
       <div className="p-4">
         <button
-          className='btn btn-secondary btn-soft btn-xl'
+          className='btn btn-secondary btn-soft btn-xl w-full lg:w-auto text-nowrap'
         >
           Play Acquire
         </button>
@@ -25,121 +25,130 @@ function App() {
           Leaderboard
         </h2>
 
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
-
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
 
 
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
+      <div 
+        className="grid grid-cols-1 lg:grid-cols-2 gap-4"
+      >
+      <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
+
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
 
 
-
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
 
 
 
-
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
 
 
 
 
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
 
 
 
 
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
-          </div>
-        </div>
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
+
+
+
+
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                <p>Leaderboard coming soon....</p>
+              </div>
+            </div>
 
 
 
 
 
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-           
+            <div 
+              className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+            >
+              <div className="card-body">
+                <h2 className="card-title">Leaderboard</h2>
+                
 
 
 
+                      <div className="chat chat-start">
+                  <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+                  </div>
                   <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+                  <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+                  </div>
+                  <div className="chat chat-start">
+                  <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
+                  </div>
+                  <div className="chat chat-start">
+                  <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
+                  </div>
+                  <div className="chat chat-end">
+                  <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+                  </div>
+                  <div className="chat chat-end">
+                  <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+                  </div>
+                  <div className="chat chat-end">
+                  <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+                  </div>
+                  <div className="chat chat-end">
+                  <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+                  </div>
               </div>
-              <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
-              </div>
-              <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
-              </div>
-              <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
-              </div>
-              <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
-              </div>
-              <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
-              </div>
-              <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
-              </div>
-              <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
-              </div>
+            </div>
           </div>
         </div>
-      </div>
+
+
+        
     </div>
   )
 }
