1970fd3ae133343b9aa255579f4051426a27bbe7
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-02-23 20:17:44 -0600

    Played with DaisyUI chat bubbles

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,34 @@ function App() {
         >
           <div className="card-body">
             <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard coming soon....</p>
+           
+
+
+
+                  <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+              </div>
+              <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+              </div>
+              <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
+              </div>
+              <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
+              </div>
+              <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+              </div>
+              <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+              </div>
+              <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+              </div>
+              <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+              </div>
           </div>
         </div>
       </div>
