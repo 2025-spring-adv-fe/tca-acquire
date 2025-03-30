4cb9615cf7f589ea0f5aabcc6fa7aa5b991e132c
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-03-30 16:43:52 -0500

    Showing the feature in JSX
    Need to hook up + button next

--- src/Play.tsx
+++ src/Play.tsx
@@ -11,6 +11,13 @@ import { useNavigate } from "react-router";
   >
     Play
   </h3>
+
+  <h4 className="text-lg font-semibold">
+    Turn #1
+    <button className="btn btn xs-outline btn-light ml-4">
+      +
+    </button>
+  </h4>
   <button className="btn btn active btn-secondary btn-lg mt-4"
     onClick= {
       () => nav3(-2)
