bd240177e38bb274e44d38e7d741d5f90291cf1f
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-04-06 16:24:10 -0500

    mapped over leaderboard

--- src/Home.tsx
+++ src/Home.tsx
@@ -31,9 +31,36 @@ export const Home: React.FC<HomeProps> = ({
             <div className="card w-full bg-base-100 card-md shadow-lg mt-4">
                 <div className="card-body">
                     <h2 className="card-title">Leaderboard</h2>
-                    <p>
-                        Leaderboard goes here...
-                    </p>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>W</th>
+                                    <th>L</th>
+                                    <th>AVG</th>
+                                    <th>PLAYER</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              {
+                                leaderboardData.map(
+                                  x => (
+                                    <tr>
+                                    <th>{x.wins}</th>
+                                    <td>{x.losses}</td>
+                                    <td>{x.average}</td>
+                                    <td>{x.player}</td>
+                                </tr>
+                                  )
+                                )
+                              }
+
+
+                      
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </>
