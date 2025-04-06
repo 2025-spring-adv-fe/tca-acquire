63efd26a0adc7f5de828470c3c1e27d12e4b0e8d
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-04-06 16:01:29 -0500

    Plumbing working for leaderboard

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router";
+import { LeaderboardEntry } from "./GameResults";
 
 interface HomeProps {
   totalGameCount: number;
+  leaderboardData: LeaderboardEntry[];
 }
 
- export const Home: React.FC<HomeProps> = ({totalGameCount}) => {
+ export const Home: React.FC<HomeProps> = ({totalGameCount, leaderboardData}) => {
+
+  console.log(leaderboardData);
 
   // Use a react hook for button navigation
 
