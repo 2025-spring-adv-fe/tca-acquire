6d8073f6eae9b75492d3d9e93ec0d3a6c633c461
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-04-06 16:17:42 -0500

    card placeholder for leaderboard

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,32 +2,40 @@ import { useNavigate } from "react-router";
 import { LeaderboardEntry } from "./GameResults";
 
 interface HomeProps {
-  totalGameCount: number;
-  leaderboardData: LeaderboardEntry[];
+    totalGameCount: number;
+    leaderboardData: LeaderboardEntry[];
 }
 
- export const Home: React.FC<HomeProps> = ({totalGameCount, leaderboardData}) => {
+export const Home: React.FC<HomeProps> = ({
+    totalGameCount,
+    leaderboardData,
+}) => {
+    console.log(leaderboardData);
 
-  console.log(leaderboardData);
+    // Use a react hook for button navigation
 
-  // Use a react hook for button navigation
+    const nav = useNavigate();
 
-  const nav = useNavigate();
+    return (
+        <>
+            <h3 className="text-2xl font-bold">
+                Home ({totalGameCount} games played)
+            </h3>
+            <button
+                className="btn btn active btn-secondary btn-lg mt-4"
+                onClick={() => nav("/setup")}
+            >
+                Play Acquire
+            </button>
 
-  return(
-  <>
-  <h3
-    className='text-2xl font-bold'
-  >
-    Home ({totalGameCount} games played)
-  </h3>
-  <button 
-    className="btn btn active btn-secondary btn-lg mt-4"
-    onClick= {
-      () => nav("/setup")
-    }
-    >
-    Play Acquire</button>
-  </>
-  );
+            <div className="card w-full bg-base-100 card-md shadow-lg mt-4">
+                <div className="card-body">
+                    <h2 className="card-title">Leaderboard</h2>
+                    <p>
+                        Leaderboard goes here...
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 };
