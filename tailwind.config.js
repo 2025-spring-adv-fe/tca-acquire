bf086d391f1d772e626ff349d095ffa4699780cb
Maxwell Wilke <143225991+maxwellwilke@users.noreply.github.com>
2025-05-12 18:59:26 -0500

    Got Custom DaisyUI color theme hooked up...
    Will work on getting colors matched better.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,9 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        acquire: {
-          primary: "#F5DEB3",         // Off-white/yellow
-          secondary: "#D9CAB3",       // Deep blue
-          accent: "#FFD700",          // Luxor gold
-          neutral: "#2C2C2C",         // Tower black
-          "base-100": "#1C2E5B",      // Page background
-          info: "#1E90FF",            // American blue
-          success: "#228B22",         // Continental green
-          warning: "#FFA500",         // Worldwide orange
-          error: "#DC143C",           // Imperial red
-        }
-      }
-    ],
-  },
-};
+}
